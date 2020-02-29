aef36224de8011698389a7b0b2c2e505d0ebc25c
PythoninjA <41398548+Yossefalmog@users.noreply.github.com>
2020-02-29 15:01:50 +0200

    Add files via upload
    \n

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,7 +1,6 @@
 
-
 def display_board(board):
-    print('\n*10000000000')
+    print('\n' *100000)
 
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
@@ -15,7 +14,9 @@ def display_board(board):
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
+
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+
 
 def player_input():
     ''''output = player1 or player 2 marker'''
@@ -25,50 +26,59 @@ def player_input():
         marker = input('Choose X or O').upper()
 
     if marker == 'X':
-        return ('X','O')
+        return ('X', 'O')
     else:
-        return ('O','X')
+        return ('O', 'X')
+
 
 def place_marker(board, marker, position):
     board[position] = marker
 
-def win_check(board, mark):
 
+def win_check(board, mark):
     return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
-    (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
-    (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
-    (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
-    (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
-    (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
-    (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
-    (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+
 
 import random
+
+
 def choost_first():
-    flip = random.randint(0,1)
+    flip = random.randint(0, 1)
     if flip == 0:
         return 'Player1 go first'
     else:
         return 'Player2 go first'
 
+
 def space_check(board, position):
     return board[position] == ''
 
+
 def full_board_check(board):
-    for i in range(1,10):
+    for i in range(1, 10):
         if space_check(the_board, i):
             return False
     return True
 
+
 def player_choice(board):
     position = 0
-    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose a position (1-9)'))
     return position
+
 
 def replay():
     choice = input("Do you want to play again? Enter Yes/No")
     return choice == 'Yes'
+
 
 print('Welcome to TICTACTOE GAME - by Yossi Almog Maman')
 
@@ -76,7 +86,7 @@ while True:
     ##play game
 
     ##set the board
-    the_board = ['']*10
+    the_board = [''] * 10
     player1_marker, player2_marker = player_input()
 
     turn = choost_first()
@@ -97,9 +107,9 @@ while True:
         if turn == 'Player 1':
             display_board(the_board)
             position = player_choice(the_board)
-            place_marker(the_board,player1_marker,position)
+            place_marker(the_board, player1_marker, position)
 
-            if win_check(the_board,player1_marker):
+            if win_check(the_board, player1_marker):
                 display_board(the_board)
                 print('PLAYER 1 WON CONGRATULATIONS!')
                 game_on = False
@@ -130,4 +140,3 @@ while True:
 
     if not replay():
         break
-
